3dbbcdc852dac49c61b2dcd45c8fec96932a2a28
Walter Gomes <wg966@hotmail.com>
2019-05-22 15:09:25 -0300

    Adiciono mais imagens e puxar token a partir de .env

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/subosito/gotenv"
 )
 
 var (
@@ -28,15 +30,28 @@ func commandHandler(discord *discordgo.Session, message *discordgo.MessageCreate
 		"https://cdn.discordapp.com/attachments/84109753864687616/445391728212705290/C4gp0JsWYAA3zza.png",
 		"https://pbs.twimg.com/media/C8RPiHoWsAER2Gq.jpg",
 		"https://cdn.discordapp.com/attachments/84109753864687616/445391821187579914/C8_MNshXYAAgI38.png",
+		"https://pm1.narvii.com/6525/bdf76097499dd104090cc1398f8a3d28d36c5c7c_hq.jpg",
+		"https://pm1.narvii.com/6525/cb3734b8c063b13fe9640fab7f58d2dc137f037e_hq.jpg",
+		"https://pm1.narvii.com/6525/2db527c913fe3be5d7b34a3a520b7cf2e08505c0_hq.jpg",
+		"https://pm1.narvii.com/6474/ac64930de94f131c20927abedd1e7b6a2395cd02_hq.jpg",
+		"https://pm1.narvii.com/6474/f48c687672e4f3ffe7c7c7db8cf35a552ae8dd48_hq.jpg",
+		"https://media1.tenor.com/images/f54e07d2285bfea75a29b7e9ac82e70a/tenor.gif",
+		"https://pbs.twimg.com/media/C8_NeHKXkAAP31D.jpg",
+		"https://i.pinimg.com/474x/2c/7c/b6/2c7cb632e7f6d1b23483d17fbc8d598e.jpg",
 	}
 	if message.Content == "... esqueleto" {
 		discord.ChannelMessageSend(message.ChannelID, imageURLs[rand.Intn(len(imageURLs))])
 	}
 }
 
+func init() {
+	gotenv.Load()
+}
+
 func main() {
 	rand.Seed(time.Now().Unix())
-	discord, err := discordgo.New("Bot token")
+	token := os.Getenv("SKELETOKEN")
+	discord, err := discordgo.New(fmt.Sprintf("Bot %v", token))
 	if err != nil {
 		fmt.Println("Deu ruim painho")
 	}
